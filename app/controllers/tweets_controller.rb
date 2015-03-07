99b488aaaf3014f903ec7599009b26492f03b517
joanne <joannekoong@college.harvard.edu>
2015-03-07 16:02:54 -0500

    newsfeed

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,17 +4,22 @@ class TweetsController < ApplicationController
 	end
 	
 	def create
-		@tweet = Tweet.new(params[tweet_params])
+		@tweet = Tweet.new(tweet_params)
 		if @tweet.save 
 			flash[:success] = "you have created a tweet!"
 			redirect_to new_tweet_path
-		end 
-		
+		end 		
 	end
+
+	def index 
+		@tweets = Tweet.all 
+	end
+
 
 	def tweet_params
 		params.require(:tweet).permit(:content)
 	end
+
 
 
 end
